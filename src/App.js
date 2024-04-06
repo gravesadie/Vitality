16f20845901a4207c7fde8144dcd2123b2d23d75
gravesadie <gravesadie@gmail.com>
2024-04-06 13:32:08 -0700

    improving folder organization

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './styles.css';
+import './App.css';
 
 function App() {
   return (
@@ -24,3 +24,30 @@ function App() {
 }
 
 export default App;
+
+/*import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+*/
