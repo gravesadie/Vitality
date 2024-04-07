397897c533cc2b078921a1a5de90100d62595bd1
gravesadie <gravesadie@gmail.com>
2024-04-06 23:00:17 -0700

    webpage edits

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef} from 'react';
 import Logo from './vector3.svg';
 import Splash from './website_photo.jpg';
+import Cloud from './ReSoul.png';
 import './App.css';
 import axios from 'axios';
 
@@ -58,24 +59,41 @@ function App() {
       </div>
 
       <div className="Introduction">
-        <h2> Motivation </h2>
+        <h2> Welcome to ReSoul </h2>
         <p>
-          MOTIVATION, SUSTAINABILITY, WELLNESS.
+          ReSoul was created with three central pillars in mind: sustainability, wellness, and business integration.
+          Repairing climbing shoes means less wasted equipment in landfill.
+          ReSoul enables novice and experienced climbers alike to invest in their wellness through rock climbing. 
+          As an excellent source of exercise that can easily be done inside or outdoors,
+          rock climbing has become a crucial way for many to relax, play, and connect with others.
+          By providing a digital platform for users to identify local repair services, we are providing a more seamless
+          retail experience that reaches a broader customer base.
         </p>
       </div>
 
       <div className="Resoling">
-        <h2> What Is Resoling? </h2>
-        <p>
-          What resoling is, why it's important.
-        </p>
-        <div className="Image">
-          <img className="before-after-image" src={"https://blog.weighmyrack.com/wp-content/uploads/2019/12/Yosemite-Bum-Resoling-before-after-Photo.jpg"} alt={`Before & after resoling climbing shoes`} />
+        <div className='ResolingText'>
+          <h2> What Is Resoling? </h2>
+          <p>
+            Over time, the rubber used to give climbing shoes their grip naturally wears down.
+            Sadly, your climbing shoes won't last forever, but with ReSoul, you can make them last a little longer.
+            Using ReSoul, you can upload a photo of your climbing shoes and our AI-trained image recognition model
+            will identify if your shoes require resoling. Resoling is the process of replacing the worn rubber 
+            on the front and/or bottom of your climbing shoes. Resoling can prolong the lifetime of your favourite 
+            climbing shoes by months or even years, and is a worthwhile investment in your quality climbing apparel.
+          </p>
+        </div>
+        
+        <div className="ResolingImage">
+          <img className="before-after-image" src={"https://blog.weighmyrack.com/wp-content/uploads/2019/12/Yosemite-Bum-Resoling-before-after-Photo.jpg"} 
+          alt={`Before & after resoling climbing shoes`}
+          width={"1000px"} />
         </div>
       </div>
       
       <div className="Upload">
-        <h3> Upload your climbing shoes: </h3>
+        <img className='uploadcloud' src={Cloud} alt="upload symbol" width={"10%"}/>
+        <h5> Drag and drop here <br></br> or </h5>
         <button onClick={handleButtonClick}>{buttonText}</button>
         <input type="file" 
         onChange={handleImageUpload}
@@ -83,7 +101,6 @@ function App() {
         style={{ display: 'none' }}/>
       </div>
 
-      {/* ONLY WANT BUTTON TO APPEAR ONCE FILE SELECTED */}
       <div className='ConfirmUpload'> 
         <button> Analyze </button>
       </div>
